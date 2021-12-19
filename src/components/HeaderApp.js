323be64224fedd1bdbbd87f16047b97c4bcec279
Angel Salazar <angel.salazar@inovtech.com.mx>
2021-12-19 19:14:51 -0300

    listado de productos en mi web

---
+++ src/components/HeaderApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+export const HeaderApp = () => (
+    <Row>
+        <Col xs={12}>
+            <div>Mini Ecommerce</div>
+        </Col>
+    </Row>
+)
