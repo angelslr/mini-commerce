4818b8070e2ab9a35fde538cf1726461b2e22d64
Angel Salazar <angel.salazar@inovtech.com.mx>
2021-12-30 18:56:49 -0300

    elimine comentarios

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -7,15 +7,6 @@ export const ListProducts = () => {
 
     const products = json.products;
 
-    /*const recorrer = (array) => {
-        const list = [];
-        for (let i = 0; array.length > i; i++) {
-            const element = <div key={i}>{array[i].title}</div>
-            list.push(element);
-        }
-        return list;
-    }*/
-
     return (
         <Row>
             {products.map((product, index) => (
